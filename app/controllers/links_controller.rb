28cdbef2109dfe692440dafad2d867dae452ebc3
Chris <clai88@gmail.com>
2016-07-29 10:18:00 -0400

    able to view links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,3 +1,25 @@
 class LinksController < ApplicationController
-  
+  def index
+    @links = Link.all
+  end
+
+  def show
+    @link = Link.find(params["id"])
+  end
+
+  def new
+    @new_link = Link.new
+
+    # redirect_to "/links"
+  end
+
+  def create
+    @link = Link.create(link_params)
+
+    redirect_to "/links/#{@link.id}"
+  end
+
+  private def link_params
+    params.require("link").permit(:title, :url)
+  end
 end
